5cc88f989bbd427353258b25168fc53f9247d9b2
Adam Rohde <adamrohde@outlook.com>
2020-11-19 17:41:48 -0600

    latest

--- res/js/index.js
+++ res/js/index.js
@@ -1,92 +1,4 @@
-
-
-//Todo 
-window.setInterval(function(){
-
-    let tDate = new Date()
-
-//------------------------------------------------------------------Day of week----------------------------------------------------
-    //Displays the day of the week
-    document.getElementById("dayoftheweek").innerHTML = dayoftheweek[tDate.getDay()];
-
-
-//------------------------------------------------------------------Time----------------------------------------------------
-    //Displays the current time in 12 hour time if radio is checked
-    if (document.getElementById('12-hour-clock').checked) {
-        //console.log("12 hour checked");
-
-        //Handles 1pm to 11:59pm or 13:00 to 23:59
-        if (tDate.getHours() > 12){
-            var temphours = tDate.getHours() - 12;
-            if (temphours < 10){
-                document.getElementById("hour").innerHTML = "0" + (tDate.getHours() - 12);
-            }else{
-                document.getElementById("hour").innerHTML = tDate.getHours() - 12;
-            }
-        //Handles 12am to 11:59am or 00:00 to 11:59   
-        }else if (tDate.getHours() < 12){    
-            if (tDate.getHours() < 10){
-                //Handles 00:00 and converts to 01:00
-                if (tDate.getHours() == 0){
-                    document.getElementById("hour").innerHTML = "01";
-                }else {
-                    document.getElementById("hour").innerHTML = "0" + (tDate.getHours());
-                }
-            }else{
-                document.getElementById("hour").innerHTML = tDate.getHours() - 12;
-            }
-        } 
-
-       
-    //Displays the current time in 24 hour time if radio is checked  
-    }else if (document.getElementById('24-hour-clock').checked){
-       // console.log("24 hour checked");
-       
-        if (tDate.getHours() < 10){
-
-            if (tDate.getHours() == 0){
-                document.getElementById("hour").innerHTML = "00";
-            }else{
-                document.getElementById("hour").innerHTML = "0" + tDate.getHours();
-            }
-
-        }else{
-            document.getElementById("hour").innerHTML = tDate.getHours();
-        }
-    }
-//---------------------------------------------------------Date----------------------------------------------------------------
-    if (tDate.getMinutes() < 10){
-        document.getElementById("minute").innerHTML = "0" + tDate.getMinutes();
-    }else{
-        document.getElementById("minute").innerHTML = tDate.getMinutes();
-    }
-
-    if (tDate.getSeconds() < 10){
-        document.getElementById("second").innerHTML = "0" + tDate.getSeconds();
-    }else{
-        document.getElementById("second").innerHTML = tDate.getSeconds();
-    }
-
-    //Displays the month
-    document.getElementById("month").innerHTML = month[tDate.getMonth()];
-
-    //Displays the date
-    document.getElementById("day").innerHTML = tDate.getDate() + ",";
-
-    //Displays the year
-    document.getElementById("year").innerHTML = tDate.getFullYear();
-
-    //Console info
-    console.log(tDate.getDay(), tDate.getHours(), tDate.getMinutes(), tDate.getSeconds())
-    console.log(dayoftheweek[tDate.getDay()]);
-    console.log(month[tDate.getMonth()]); 
-
-
-}, 1000);
-
-
-
-var dayoftheweek = new Array(7);
+const dayoftheweek = new Array(7);
 dayoftheweek[0] = "Sunday";
 dayoftheweek[1] = "Monday";
 dayoftheweek[2] = "Tuesday";
@@ -95,8 +7,7 @@ dayoftheweek[4] = "Thursday";
 dayoftheweek[5] = "Friday";
 dayoftheweek[6] = "Saturday";
 
-
-var month = new Array(12);
+const month = new Array(12);
 month[0] = "January";
 month[1] = "February";
 month[2] = "March";
@@ -110,3 +21,35 @@ month[9] = "October";
 month[10] = "November";
 month[11] = "December";
 
+function renderClock() {
+    let theDate = new Date();
+
+    //Displays the day of the week
+    document.getElementById("dayoftheweek").textContent = dayoftheweek[theDate.getDay()];
+
+    const isCivilianTime = document.getElementById("12-hour-clock").checked;
+
+    const hours = theDate.getHours();
+
+    if (isCivilianTime) {
+        const civilianHours = hours > 12 ? hours - 12 : hours;
+        document.getElementById("hour").textContent = civilianHours;
+    } else {
+        document.getElementById("hour").textContent = hours;
+    }
+
+    document.getElementById("minute").textContent = addLeadingZero(theDate.getMinutes());
+
+    document.getElementById("second").textContent = addLeadingZero(theDate.getSeconds());
+
+    document.getElementById("month").textContent = month[theDate.getMonth()] + ",";
+    document.getElementById("day").textContent = theDate.getDate() + ",";
+    document.getElementById("year").textContent = theDate.getFullYear();
+}
+
+function addLeadingZero(time) {
+    return time < 10 ? "0" + time : time;
+}
+
+renderClock();
+window.setInterval(renderClock, 1000);
